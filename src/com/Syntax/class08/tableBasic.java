82c6474808e97f8a9d1e1867542f231bc9e45c94
user.email <charlesguevara273@gmail.com>
2022-11-15 23:46:46 -0500

    class 8 and review

--- src/com/Syntax/class08/tableBasic.java
+++ src/com/Syntax/class08/tableBasic.java
@@ -1,2 +1,48 @@
-package com.Syntax.class08;public class tableBasic {
+package com.Syntax.class08;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.util.List;
+
+public class tableBasic {
+    public static void main(String[] args) {
+        //        set the path to the driver to link it with our class    on mac u dont need .exe on windows u need .exe
+        System.setProperty("webdriver.edge.driver", "Drivers/msedgedriver.exe");
+        // create a WebDriver instance
+        WebDriver driver = new EdgeDriver();
+//      maximize window
+        driver.manage().window().maximize();
+        driver.get("https://www.techlistic.com/p/demo-selenium-practice.html");
+
+        //        print the whole table on the console
+
+        WebElement allTable = driver.findElement(By.xpath("//table[@id='customers']"));
+        String textAllTable = allTable.getText();
+        System.out.println(textAllTable);
+
+        //        print all the rows of the table
+        List<WebElement> rows = driver.findElements(By.xpath("//table[@id='customers']/tbody/tr"));
+        for (WebElement row : rows) {
+            String rowText = row.getText();
+//            System.out.println(rowText);
+//            System.out.println("-----------------");
+//             modified task : print only the row which has company google
+            if (rowText.contains("Google")) {
+                System.out.println(rowText);
+            }
+
+
+            //        print all the individual columns of the table
+            List<WebElement> columns = driver.findElements(By.xpath("//table[@id='customers']/tbody/tr/td"));
+            for (WebElement column : columns) {
+                String columntext = column.getText();
+                System.out.println(columntext);
+                System.out.println("-----------------");
+            }
+
+        }
+    }
 }
--- src/com/Syntax/class08/waitsDemo.java
+++ src/com/Syntax/class08/waitsDemo.java
@@ -1,2 +1,49 @@
-package com.Syntax.class08;public class waitsDemo {
+package com.Syntax.class08;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+public class waitsDemo {
+    public static void main(String[] args) {
+        //        set the path to the driver to link it with our class    on mac u dont need .exe on windows u need .exe
+        System.setProperty("webdriver.edge.driver", "Drivers/msedgedriver.exe");
+        // create a WebDriver instance
+        WebDriver driver = new EdgeDriver();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+//      maximize window
+        driver.manage().window().maximize();
+        driver.get("https://chercher.tech/practice/explicit-wait-sample-selenium-webdriver");
+
+//       click on button "change text to selenium webDriver"
+        WebElement button1 = driver.findElement(By.xpath("//button[@id = 'populate-text']"));
+        button1.click();
+
+//         get the text of Selenium webDriver
+        WebElement webDriverText = driver.findElement(By.xpath("//h2[text() = 'Selenium Webdriver']"));
+
+//       print out the text on the screen
+        System.out.println(webDriverText.getText());
+
+//  Task2
+//           click on the button "display button after 10 seconds"
+        WebElement btn2 = driver.findElement(By.xpath("//button[@id = 'display-other-button']"));
+        btn2.click();
+
+//        as the button appears up after some time so in order to click it or get text from
+//         it we need to define some explicit wait
+
+        WebDriverWait wait = new WebDriverWait(driver,20);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@id = 'hidden']")));
+
+        WebElement visiblebtn = driver.findElement(By.xpath("//button[@id = 'hidden']"));
+        visiblebtn.click();
+
+
+
+    }
 }
